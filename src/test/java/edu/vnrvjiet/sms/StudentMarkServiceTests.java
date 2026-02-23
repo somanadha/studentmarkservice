9e3dd49442eef50c77d464872cfc5bcbf491fbef
Somanadha Satyadev Bulusu <soma_satya@yahoo.com>
2026-02-23 13:09:30 +0530

    Testing

--- src/test/java/edu/vnrvjiet/sms/StudentMarkServiceTests.java
+++ src/test/java/edu/vnrvjiet/sms/StudentMarkServiceTests.java
@@ -13,7 +13,6 @@ import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 @SpringBootTest
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
-@Suite
 public class StudentMarkServiceTests {
 
     @Autowired
